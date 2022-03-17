15cf452418e697b2ec838e4e521b67411cffb193
yedeka <yogesh.edekar@gmail.com>
2022-03-16 18:10:37 -0700

    Adding logic for set matrix zeros space optimized approach.

--- src/com/tuf/days/day1/arrays/Set_Matrix_Zeros.java
+++ src/com/tuf/days/day1/arrays/Set_Matrix_Zeros.java
@@ -70,8 +70,24 @@ public class Set_Matrix_Zeros {
                 }
             }
         }
+    }
 
+    private static void setZeroes_SE(int[][] matrix) {
+        int col0 = 1, rows = matrix.length, cols = matrix[0].length;
 
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
     }
 
     private static void printMatrix(int[][] matrix){
@@ -88,8 +104,10 @@ public class Set_Matrix_Zeros {
                 {1, 0, 1},
                 {1, 1, 1}
         };
+        matrix = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
         //setMatrixZeros_Brute_Force(matrix);
-        nonBruteForceLogic(matrix);
+        // nonBruteForceLogic(matrix);
+        setZeroes_SE(matrix);
         printMatrix(matrix);
 
     }
