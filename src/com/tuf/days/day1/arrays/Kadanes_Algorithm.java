62dbe54514fb5a55e50b744aa4a02ef53db985be
yedeka <yogesh.edekar@gmail.com>
2022-03-16 21:53:38 -0700

    Adding more test cases for kadane's

--- src/com/tuf/days/day1/arrays/Kadanes_Algorithm.java
+++ src/com/tuf/days/day1/arrays/Kadanes_Algorithm.java
@@ -12,6 +12,7 @@ public class Kadanes_Algorithm {
     public static void main(String[] args){
         int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
         nums = new int[]{1, 2, 7, -4, 3, 2, -10, 9,1};
+        nums = new int[]{-7, -8, -16, -4, -8, -5, -7, -11, -10, -12, -4, -6, -4, -16, -10};
         System.out.println("Maximum subarray sum => "+findMaxSubArraySum(nums));
     }
 }
