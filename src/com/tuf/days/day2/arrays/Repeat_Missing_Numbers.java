62de56b4e0ad47fed3f9240af5feb748d5cf30c6
yedeka <yogesh.edekar@gmail.com>
2022-03-18 12:45:20 -0700

    Adding Notes.

--- src/com/tuf/days/day2/arrays/Repeat_Missing_Numbers.java
+++ src/com/tuf/days/day2/arrays/Repeat_Missing_Numbers.java
@@ -1,6 +1,8 @@
 package com.tuf.days.day2.arrays;
 
 public class Repeat_Missing_Numbers {
+    // Time complexity - O(N)
+    // Space complexity - O(N) -  we need an auxilary array to store counting sort.
     private static int[] findRepeatMissing_Brute_Force(int[] nums){
         int[] result = new int[2];
         //Use counting sort with an auxillary array.
@@ -18,9 +20,35 @@ public class Repeat_Missing_Numbers {
         }
         return result;
     }
+
+    // Time complexity - o(N)
+    // Space complexity - O(1) since we dont take any extra space.
+    private static int[] usingSeries(int[] nums){
+        int length = nums.length;
+        long sum = (length * (length + 1))/2;
+        long squareSum = sum * (2*length + 1)/3;
+
+        for(int i=0; i<length; i++){
+            sum -= nums[i];
+            squareSum -= (nums[i] * nums[i]);
+        }
+
+        long missing = (sum + (squareSum / sum))/2;
+        long repeating = missing - sum;
+
+        int[] result = new int[]{(int)repeating, (int)missing};
+        return result;
+    }
+
+
+
     public static void main(String[] args){
         int[] nums = new int[]{3, 1, 2, 5, 3};
+        // Counting sort usage
         int[] result = findRepeatMissing_Brute_Force(nums);
         System.out.println(result[0]+", "+result[1]);
+        result = usingSeries(nums);
+        System.out.println(result[0]+", "+result[1]);
+
     }
 }
