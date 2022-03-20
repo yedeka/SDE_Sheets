8ec5c00c2d6fb6e98cfb6a697d036b0ef4fe40ea
yedeka <yogesh.edekar@gmail.com>
2022-03-19 18:05:14 -0700

    Adding logic for optimal solution for grid path problem.

--- src/com/tuf/days/day3/arrays/Grid_Unique_Paths.java
+++ src/com/tuf/days/day3/arrays/Grid_Unique_Paths.java
@@ -34,11 +34,27 @@ public class Grid_Unique_Paths {
         return result;
     }
 
+    // The best appraoch is using combinations.
+    // Simply observe the pattern to find out the combinations for the result
+    // Time Complexity O(m-1)
+    // Space Complexity O(1)
+    private static int findUniquePaths_Combinatrics(int m, int n){
+        int N = m + n - 2;
+        int r = m - 1;
+        double res = 1;
+
+        for(int i=1; i<=r; i++){
+            res *= (N-r+i)/i;
+        }
+        return (int)res;
+    }
+
     public static void main(String[] args){
         int m = 2, n = 3;
         System.out.println("Unique paths => "+findUniquePaths(0, 0, m, n));
         int[][] dp = new int[m][n];
         Arrays.stream(dp).forEach(arr -> Arrays.fill(arr, -1));
         System.out.println("DP unique paths => "+findUniquePathsDP(0, 0, m, n, dp));
+        System.out.println("Combinatrics unique paths => "+findUniquePaths_Combinatrics(m, n));
     }
 }
