4b3293e9332d211dd70a8fddf4d9826ba35796ca
yedeka <yogesh.edekar@gmail.com>
2022-03-22 20:54:30 -0700

    Adding code for remove nth node from end.

---
+++ src/com/tuf/days/day5/linkedlist/RemoveNthNodeFromEnd.java
@@ -0,0 +1,53 @@
+package com.tuf.days.day5.linkedlist;
+
+public class RemoveNthNodeFromEnd {
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    private static Node removeNthEnd(Node head, int n){
+        Node returnHead = null;
+        // Step 1 take aheadPointer and move it ahead by N nodes
+        Node behindPtr=head, aheadPtr = head, prev=null;
+        while(n-- > 0){
+            aheadPtr = aheadPtr.next;
+        }
+        while(aheadPtr != null){
+            prev = behindPtr;
+            behindPtr = behindPtr.next;
+            aheadPtr = aheadPtr.next;
+        }
+
+        Node next = behindPtr.next;
+        if(prev != null){
+            prev.next = next;
+            returnHead = head;
+        } else {
+            returnHead = next;
+        }
+
+        return returnHead;
+    }
+
+    public static void main(String[] args){
+        Node head = new Node(1);
+        Node first = new Node(2);
+        Node second = new Node(3);
+        Node third = new Node(4);
+        Node fourth = new Node(5);
+
+        head.next = first;
+        first.next = second;
+        second.next = third;
+        third.next = fourth;
+        int n = 2;
+
+        Node newHead = removeNthEnd(head, n);
+        System.out.println("DONE");
+    }
+}
