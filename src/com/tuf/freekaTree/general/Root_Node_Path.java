64128654a3e6d4580d99412224a3b5bcaad7b4b9
yedeka <yogesh.edekar@gmail.com>
2022-04-02 11:52:20 -0700

    Adding code for root to node path.

---
+++ src/com/tuf/freekaTree/general/Root_Node_Path.java
@@ -0,0 +1,56 @@
+package com.tuf.freekaTree.general;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Root_Node_Path {
+    private static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    private static boolean findRootToNodePath(Node root, List<Integer> result, int searchVal){
+        if(root == null){
+            return false;
+        }
+        result.add(root.data);
+        if(root.data == searchVal){
+            return true;
+        }
+        boolean leftval = findRootToNodePath(root.left, result, searchVal);
+        boolean rightval = findRootToNodePath(root.right, result, searchVal);
+
+        if(leftval || rightval){
+            return true;
+        }
+        result.remove(result.size() - 1);
+        return false;
+    }
+
+    public static void main(String[] args){
+        Node root = new Node(1);
+        Node left = new Node(2);
+        Node right = new Node(3);
+        Node leftLeft2 = new Node(4);
+        Node leftRight2 = new Node(5);
+        Node leftLeft3 = new Node(6);
+        Node leftRight3 = new Node(7);
+
+        root.left = left;
+        root.right = right;
+        left.left = leftLeft2;
+        left.right = leftRight2;
+        leftRight2.left = leftLeft3;
+        leftRight2.right = leftRight3;
+
+        List<Integer> rootNodeList = new ArrayList<>();
+        findRootToNodePath(root, rootNodeList, 7);
+        System.out.println(rootNodeList);
+
+    }
+
+}
