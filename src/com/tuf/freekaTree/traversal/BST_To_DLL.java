c409f088c75ad8d85f3ab77f8964c06210c98674
yedeka <yogesh.edekar@gmail.com>
2022-04-14 17:35:48 -0700

    Adding code for BST to Doubly Linked List.

--- src/com/tuf/freekaTree/traversal/BST_To_DLL.java
+++ src/com/tuf/freekaTree/traversal/BST_To_DLL.java
@@ -11,6 +11,22 @@ public class BST_To_DLL {
         }
     }
 
+    private static void BSTDLLHelper(Node root, Node[] prev){
+        if(root == null) return;
+        BSTDLLHelper(root.left, prev);
+        root.left = prev[0];
+        prev[0].right = root;
+        prev[0] = root;
+        BSTDLLHelper(root.right, prev);
+    }
+
+    private static Node convertBSTTODLL(Node root){
+        Node dummy = new Node(-1);
+        Node[] prev = new Node[]{dummy};
+        BSTDLLHelper(root, prev);
+        return dummy;
+    }
+
     private static Node preapreTree(){
         Node root = new Node(25);
         Node left = new Node(20);
@@ -24,11 +40,25 @@ public class BST_To_DLL {
         left.right = leftRight;
         Node leftLeft1 = new Node(5);
         Node leftRight1 = new Node(12);
+        leftLeft.left = leftLeft1;
+        leftLeft.right = leftRight1;
+        // Right subtree
+        Node rightLeft1 = new Node(30);
+        Node rightRight1 = new Node(40);
+        right.left = rightLeft1;
+        right.right = rightRight1;
+        Node rightLeft2 = new Node(28);
+        rightLeft1.left = rightLeft2;
+        Node rightLeft21 = new Node(38);
+        Node rightRight21 = new Node(48);
+        rightRight1.left = rightLeft21;
+        rightRight1.right = rightRight21;
 
         return root;
     }
 
     public static void main(String[] args){
-
+        Node root = preapreTree();
+        convertBSTTODLL(root);
     }
 }
