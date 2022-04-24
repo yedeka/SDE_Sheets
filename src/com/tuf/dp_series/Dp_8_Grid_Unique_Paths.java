efca48522cef42cae64607532ec71f3c39dc1015
yedeka <yogesh.edekar@gmail.com>
2022-04-23 20:16:22 -0700

    Adding tabulation and tabulation space optimized solutions.

--- src/com/tuf/dp_series/Dp_8_Grid_Unique_Paths.java
+++ src/com/tuf/dp_series/Dp_8_Grid_Unique_Paths.java
@@ -27,6 +27,51 @@ public class Dp_8_Grid_Unique_Paths {
         }
     }
 
+    private static int findUniqueTabulation(int n){
+        int[][] dp = new int[n][n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                int up = 0, left = 0;
+                if(j > 0){
+                    left = dp[i][j-1];
+                }
+                if(i > 0){
+                    up = dp[i-1][j];
+                }
+                dp[i][j] = up + left;
+            }
+        }
+        return dp[n-1][n-1];
+    }
+
+    private static int findUniqueTabulation_Optimized(int n){
+        int[] dp = new int[n];
+        for(int i=0; i<n; i++){
+            int[] temp = new int[n];
+            for(int j=0; j< n; j++){
+                if(i == 0 && j == 0){
+                    temp[j] = 1;
+                    continue;
+                }
+                int left = 0, up = 0;
+                if(i > 0){
+                    up = dp[j];
+                }
+                if(j > 0){
+                   left = temp[j-1];
+                }
+                temp[j] = up + left;
+            }
+            dp = temp;
+        }
+        return dp[n-1];
+    }
+
+
     private static int findUniquePathsStart(int i, int j, int n){
         if(i == n-1 && j == n-1){
             return 1;
@@ -58,6 +103,8 @@ public class Dp_8_Grid_Unique_Paths {
         System.out.println("Unique paths starting from the end => "+findUnique(n, true));
         System.out.println("Unique paths starting from the start => "+findUnique(n, false));
         System.out.println("Unique paths starting from the end memoized => "+findUniqueMemoized(n));
+        System.out.println("Unique paths using tabulation => "+findUniqueTabulation(n));
+        System.out.println("Unique paths using optimized tabulation => "+findUniqueTabulation_Optimized(n));
     }
 
 }
