7cc97ee028f11036af9c09fc0eba63e82edfdd8d
yedeka <yogesh.edekar@gmail.com>
2022-04-27 04:42:19 -0700

    Adding code for tabulation and space optimzied tabulation.

--- src/com/tuf/dp_series/subsequence_dp/DP17_Count_Subsets_With_Target_Difference.java
+++ src/com/tuf/dp_series/subsequence_dp/DP17_Count_Subsets_With_Target_Difference.java
@@ -48,6 +48,54 @@ public class DP17_Count_Subsets_With_Target_Difference {
         return dp[index] = excludeElement + includeElement;
     }
 
+    private static int count_subsets_target_sum_tabulation(int[] nums, int target){
+        int n = nums.length, m = target+1;
+        int[][] dp = new int[n][m];
+        // Handle base cases - Case 1 for any index sum of 0 gives a count of 1 i.e. 0th column of all rows is 1
+        for(int i=0; i<n; i++){
+            dp[i][0] = 1;
+        }
+        // Base case 2 - At index 0 for sum of nums[0] count is 1
+        dp[0][nums[0]] = 1;
+        // Now start iterating over the array
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                int notTaken = dp[i-1][j];
+                int taken = 0;
+                if(nums[i]<=j){
+                    taken = dp[i-1][j-nums[i]];
+                }
+                dp[i][j] = notTaken + taken;
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
+    private static int count_subsets_target_sum_tabulation_spaceOptimized(int[] nums, int target){
+        int n = nums.length, m = target+1;
+        int[] dp = new int[m];
+        // Handle base cases - Case 1 for any index sum of 0 gives a count of 1 i.e. 0th column of all rows is 1
+        for(int i=0; i<n; i++){
+            dp[0] = 1;
+        }
+        // Base case 2 - At index 0 for sum of nums[0] count is 1
+        dp[nums[0]] = 1;
+        // Now start iterating over the array
+        for(int i=1; i<n; i++){
+            int[]temp = new int[m];
+            for(int j=0; j<m; j++){
+                int notTaken = dp[j];
+                int taken = 0;
+                if(nums[i]<=j){
+                    taken = dp[j-nums[i]];
+                }
+                temp[j] = notTaken + taken;
+            }
+            dp = temp;
+        }
+        return dp[m-1];
+    }
+
     private static int count_subsets_zero_sum(int[] nums, int target, boolean recursive){
         int index = nums.length-1;
         if(recursive){
@@ -62,5 +110,7 @@ public class DP17_Count_Subsets_With_Target_Difference {
         int target = 3;
         System.out.println("Count of subsets with target sum recursive => "+count_subsets_zero_sum(nums, target, true));
         System.out.println("Count of subsets with target sum using memoization => "+count_subsets_zero_sum(nums, target, false));
+        System.out.println("Count of subsets with target sum using tabulation => "+count_subsets_target_sum_tabulation(nums, target));
+        System.out.println("Count of subsets with target sum using tabulation space optimized => "+count_subsets_target_sum_tabulation_spaceOptimized(nums, target));
     }
 }
